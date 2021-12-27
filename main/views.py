f18a38a861efb8f3057605b3e481fe3b0d7d9ac5
FireWood <sss@sss.ss>
2021-12-27 12:30:52 +0300

    Update 27.12.2021

--- main/views.py
+++ main/views.py
@@ -35,7 +35,7 @@ def listings(request):
     unit_type = request.GET.get('unit_type')
     view = request.GET.get('view')
     if unit_type or view:
-        listing_object = Listing.objects.filter(Q(UNIT_TYPE__icontains=unit_type) & Q(VIEW__icontains=view))
+        listing_object = Listing.objects.filter(Q(UNIT_TYPE__icontains=unit_type) & Q(BATHROOMS__icontains=view))
         return render(request, 'main/listings.html', context={'listing_object': listing_object})
     else:
         listing_object = Listing.objects.all()
